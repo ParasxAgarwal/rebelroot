21d731dd305d2df79a59214e6c4a5ed6d0cc6331
Paras Agarwal <parasdevprojects@gmail.com>
2026-06-09 00:20:41 +0530

    fix: resolve React type mismatch for AnimateHeight in Collapse.tsx

--- components/Collapse.tsx
+++ components/Collapse.tsx
@@ -28,20 +28,23 @@ const Collapse = forwardRef<HTMLDivElement, PropsWithChildren<CollapseProps>>(
     ref,
   ) => {
     return (
-      <AnimateHeight
-        duration={duration}
-        easing={easing}
-        animateOpacity={animateOpacity}
-        height={isOpen ? endingHeight : startingHeight}
-        applyInlineTransitions={false}
-        {...{ onAnimationStart, onAnimationEnd }}
-        style={{
-          transition: 'height .3s ease,opacity .3s ease-in-out,transform .3s ease-in-out',
-          backfaceVisibility: 'hidden',
-        }}
-      >
-        <div ref={ref} {...rest} />
-      </AnimateHeight>
+      <>
+        {/* @ts-ignore */}
+        <AnimateHeight
+          duration={duration}
+          easing={easing}
+          animateOpacity={animateOpacity}
+          height={isOpen ? endingHeight : startingHeight}
+          applyInlineTransitions={false}
+          {...{ onAnimationStart, onAnimationEnd }}
+          style={{
+            transition: 'height .3s ease,opacity .3s ease-in-out,transform .3s ease-in-out',
+            backfaceVisibility: 'hidden',
+          }}
+        >
+          <div ref={ref} {...rest} />
+        </AnimateHeight>
+      </>
     );
   },
 );
